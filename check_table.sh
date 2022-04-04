1de28272539eeead953831a91c89f0a94b0bc858
ratamon-tech <ratamon91@gmail.com>
2022-04-05 08:35:24 +0900

    修正

--- check_table.sh
+++ check_table.sh
@@ -11,5 +11,7 @@ echo '----------'
 # 1列目をキーにして連想配列で合計値を出す
 awk -F ',' '{print;x[$1]+=$2;y[$1]+=$3}END{for(k in x){print k"合計",x[k],y[k]}}' table.csv
 
-# 5列目に2,3列目を足した4列目の四捨五入した値を出す
-awk -F ',' '{print $0,$2+$3}' table.csv | ruby -ane '$F[5]=$F[4].to_f.round(1);puts $F.join(" ")'
+echo '----------'
+
+# 5列目に2,3列目を足した4列目の合計値を出す
+awk -F ',' '{print $0,$2+$3}' table.csv | ruby -ane '$F[4]=$F[3];puts $F.join(" ")'
