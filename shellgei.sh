58a5632a3a011c5911924f331195a20b62155870
ratamon <ratamon91@gmail.com>
2022-04-11 17:38:57 +0900

    データ連結追加

--- shellgei.sh
+++ shellgei.sh
@@ -1,2 +1,9 @@
 # ファイルを探す
 find shellgei160/ | grep files
+
+# データの連結
+readonly local MASTER="shellgei160/qdata/practice/master.txt"
+readonly local TRANSACTION="shellgei160/qdata/practice/transaction.txt"
+
+# マスター分のみ
+join ${MASTER} ${TRANSACTION}
